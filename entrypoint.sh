c740a3da404ca72c7f10599dd3c6cd869acdb9cf
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2025-10-22 09:02:34 -0700

    added entrypoint.sh with Dockerfile

---
+++ entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Populate database
+python alcf/database/ingestion/ingest_static_data.py
+
+# Update (if needed) the status of resources
+python alcf/database/ingestion/ingest_activity_data.py
+
+# Run application
+gunicorn app.main:app -c gunicorn.config.production.py -k uvicorn.workers.UvicornWorker
