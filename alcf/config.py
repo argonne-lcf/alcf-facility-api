48b22e2d89e9f64bdac5e1fb4ac9a2f7127c583b
BenoitCote <benoitcote@anlvpn070.mcs.anl.gov>
2025-11-11 16:49:26 -0800

    added Keycloak config envs

--- alcf/config.py
+++ alcf/config.py
@@ -13,3 +13,8 @@ SECRET_DEV_KEY = os.getenv("SECRET_DEV_KEY", None)
 
 # PBS GraphQL API URL
 GRAPHQL_URL = os.getenv("GRAPHQL_URL", "")
+
+# Keycloak integration
+KEYCLOAK_CLIENT_ID = os.getenv("KEYCLOAK_CLIENT_ID", None)
+KEYCLOAK_CLIENT_SECRET = os.getenv("KEYCLOAK_CLIENT_SECRET", None)
+KEYCLOAK_REALM_NAME = os.getenv("KEYCLOAK_REALM_NAME", None)
