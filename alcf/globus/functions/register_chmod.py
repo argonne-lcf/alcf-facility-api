46dbf63601bebfe5b5141a9b51e9c803450f3a31
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2025-12-09 17:00:13 -0800

    light space edits to functions

--- alcf/globus/functions/register_chmod.py
+++ alcf/globus/functions/register_chmod.py
@@ -27,18 +27,14 @@ def chmod(params):
     class InputData(BaseModel):
         path: str
         mode: str = Field(min_length=3, max_length=4)
-        
-        # No extra argument
         model_config = ConfigDict(extra="forbid")
 
         # Forbidden characters (prevent shell injection)
         @field_validator("path", "mode")
         @classmethod
         def forbidden_characters(cls, v: str) -> str:
-            if not re.compile(r"^[\w\-. /\\]+$").fullmatch(v):
+            if not re.compile(r"^[\w\-./\\]+$").fullmatch(v):
                 raise ValueError("Field contains forbidden characters.")
-            if " " in v:
-                raise ValueError("No empty space allowed.")
             if "\0" in v:
                 raise ValueError("Null byte not allowed.")
             return v
--- alcf/globus/functions/register_view.py
+++ alcf/globus/functions/register_view.py
@@ -28,18 +28,14 @@ def view(params):
         path: str
         size: Optional[int] = Field(default=None, ge=0)
         offset: Optional[int] = Field(default=None, ge=0)
-        
-        # No extra argument
         model_config = ConfigDict(extra="forbid")
 
         # Forbidden characters (prevent shell injection)
         @field_validator("path")
         @classmethod
         def forbidden_characters(cls, v: str) -> str:
-            if not re.compile(r"^[\w\-. /\\]+$").fullmatch(v):
+            if not re.compile(r"^[\w\-./\\]+$").fullmatch(v):
                 raise ValueError("Field contains forbidden characters.")
-            if " " in v:
-                raise ValueError("No empty space allowed.")
             if "\0" in v:
                 raise ValueError("Null byte not allowed.")
             return v
