2df8bcace8f8e8a9c63ca92eae7aeb99fcf46ec4
BenoitCote <benoitcote@anlvpn172.mcs.anl.gov>
2026-01-13 12:41:34 -0800

    updated base URL

--- alcf/python_example_scripts/compute_cancel_job.py
+++ alcf/python_example_scripts/compute_cancel_job.py
@@ -14,7 +14,7 @@ headers = {
 }
 
 # Build URL
-url = f"http://localhost:8000/api/current/compute/cancel/{resource_id}/{job_id}"
+url = f"http://localhost:8000/api/v1/compute/cancel/{resource_id}/{job_id}"
 
 # Send request to Facility API
 response = requests.delete(url, headers=headers)
