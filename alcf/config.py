d00c169ba86f2621530622a815eda8f1700d6768
BenoitCote <benoitcote@anlvpn181.mcs.anl.gov>
2026-01-14 16:22:17 -0800

    added variable to disable http proxy if needed when querying GraphQL (needed with current k8s cluster)

--- alcf/config.py
+++ alcf/config.py
@@ -14,6 +14,7 @@ SECRET_DEV_KEY = os.getenv("SECRET_DEV_KEY", None)
 
 # PBS GraphQL API URL
 GRAPHQL_URL = os.getenv("GRAPHQL_URL", "")
+GRAPHQL_HTTPX_TRUST_ENV = os.getenv("GRAPHQL_URL", "True").lower() in ("true", "1", "t")
 
 # Keycloak integration
 KEYCLOAK_CLIENT_ID = os.getenv("KEYCLOAK_CLIENT_ID", None)
