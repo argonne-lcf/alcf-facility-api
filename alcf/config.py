f3876716688c997d3da3eb8b482c675a24da3f0e
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2026-01-15 16:14:08 -0800

    added default postgres database

--- alcf/config.py
+++ alcf/config.py
@@ -4,4 +4,4 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://localhost/facilityapi")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://facilityapi_user@localhost/facilityapi_db")
