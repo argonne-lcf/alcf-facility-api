9270c317f3d9fd2b868191cf1a7a94e30d1c55f0
BenoitCote <benoitcote@anlvpn059.mcs.anl.gov>
2026-01-27 10:09:56 -0800

    added ls task submission

--- alcf/task/alcf_adapter.py
+++ alcf/task/alcf_adapter.py
@@ -1,8 +1,17 @@
+from fastapi import HTTPException
+from starlette.status import HTTP_501_NOT_IMPLEMENTED, HTTP_404_NOT_FOUND
 from app.routers.task.facility_adapter import FacilityAdapter as TaskFacilityAdapter
 from app.routers.status import models as status_models
 from app.routers.account import models as account_models
 from app.routers.task import models as task_models
 from alcf.auth.alcf_adapter import AlcfAuthenticatedAdapter
+
+# Instantiate the Filesystem adaptor
+from alcf.filesystem.alcf_adapter import AlcfAdapter as FilesystemAdaptor
+filesystem_adaptor = FilesystemAdaptor()
+filesystem_commands = {
+    "ls": filesystem_adaptor.ls
+}
 
 class AlcfAdapter(TaskFacilityAdapter, AlcfAuthenticatedAdapter):
     """Filesystem facility adapter definition for the IRI Facility API."""
@@ -22,10 +31,29 @@ class AlcfAdapter(TaskFacilityAdapter, AlcfAuthenticatedAdapter):
         pass
 
 
+    # Put task
     async def put_task(
         self: "AlcfAdapter",
         user: account_models.User,
         resource: status_models.Resource|None,
         command: task_models.TaskCommand
     ) -> str:
-        pass
+        
+        # Create input dictionary
+        kwargs = {"user": user, "resource": resource} | command.args
+
+        # Filesystem operations
+        if command.router == "filesystem":
+
+            # Supported commands
+            if command.command in filesystem_commands:
+                return await filesystem_commands[command.command](**kwargs)
+            
+            # Unsupported commands
+            else:
+                raise HTTPException(status_code=HTTP_501_NOT_IMPLEMENTED, detail="'recursive' option not implemented yet.")
+
+        # Other operations not implemented yet
+        else: 
+            raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=f"Router {command.router} not found.")
+        
