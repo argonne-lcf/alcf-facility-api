3f1bee0acfa11b9994452b2f2f7d9a0d52e64825
BenoitCote <benoitcote@anlvpn214.mcs.anl.gov>
2026-03-03 13:17:52 -0800

    made output print with indent

--- alcf/python_example_scripts/get_task.py
+++ alcf/python_example_scripts/get_task.py
@@ -1,11 +1,12 @@
 import os
 import requests
+import json
 from globus_access_token import get_access_token
 from dotenv import load_dotenv
 load_dotenv()
 
 # Define your task ID
-task_id = "2c99e2aa-b3bf-4ab7-afd3-4e7c36e0ebc8"
+task_id = "6daa409a-09a8-4b76-80ad-c9ca51e9d461"
 
 # Get Globus access token
 ACCESS_TOKEN = get_access_token()
@@ -24,4 +25,4 @@ response = requests.get(url, headers=headers)
 
 # Print response
 print(response.status_code)
-print(response.json())
+print(json.dumps(response.json(), indent=2))
