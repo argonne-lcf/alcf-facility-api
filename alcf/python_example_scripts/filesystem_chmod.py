6a0f35d4a09f5f92ec539105a768c797745d5178
BenoitCote <benoitcote@anlvpn214.mcs.anl.gov>
2026-03-03 14:44:20 -0800

    switched filesystem target to Sophia

--- alcf/python_example_scripts/filesystem_chmod.py
+++ alcf/python_example_scripts/filesystem_chmod.py
@@ -8,7 +8,8 @@ load_dotenv()
 ACCESS_TOKEN = get_access_token()
 
 # Targeted resource
-resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
+#resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
+resource_id = "9674c7e1-aecc-4dbb-bf01-c9197e027cd6" # Sophia
 
 # Build input data
 data = {
