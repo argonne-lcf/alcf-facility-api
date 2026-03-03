bcecc57847314375bffca5772c4a283498b53edc
BenoitCote <benoitcote@anlvpn214.mcs.anl.gov>
2026-03-03 15:40:18 -0800

    added python example on how to use checksum

---
+++ alcf/python_example_scripts/filesystem_checksum.py
@@ -0,0 +1,33 @@
+import os
+import requests
+from globus_access_token import get_access_token
+from dotenv import load_dotenv
+load_dotenv()
+
+# Get Globus access token
+ACCESS_TOKEN = get_access_token()
+
+# Targeted resource
+#resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
+resource_id = "9674c7e1-aecc-4dbb-bf01-c9197e027cd6" # Sophia
+
+# Build input data
+data = {
+    "path": "/home/bcote/test.txt",
+}
+
+# Build headers
+headers = {
+    "Authorization": f"Bearer {ACCESS_TOKEN}",
+    "Content-Type": "application/json"
+}
+
+# Build URL
+url = f"{os.getenv('BASE_URL')}/filesystem/checksum/{resource_id}"
+
+# Send request to Facility API
+response = requests.get(url, params=data, headers=headers)
+
+# Print response
+print(response.status_code)
+print(response.json())
