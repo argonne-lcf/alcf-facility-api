be16c095568e12166b023384055b71b1fad2c24a
BenoitCote <benoitcote@anlvpn214.mcs.anl.gov>
2026-03-03 13:17:03 -0800

    cache gcc, use batch to recover task id

--- alcf/globus/utils.py
+++ alcf/globus/utils.py
@@ -1,6 +1,7 @@
 import asyncio
 import time
 import json
+from cachetools import TTLCache, cached
 from alcf.config import GLOBUS_COMPUTE_ENDPOINTS, GLOBUS_COMPUTE_FUNCTIONS
 from starlette.status import HTTP_501_NOT_IMPLEMENTED, HTTP_500_INTERNAL_SERVER_ERROR, HTTP_408_REQUEST_TIMEOUT
 from fastapi import HTTPException
@@ -16,12 +17,12 @@ ComputeScopes = ComputeScopeBuilder()
 
 
 # Get Globus Compute Executor
-def get_compute_executor(user: account_models.User) -> Executor:
+def get_compute_executor(user_name: str, user_api_key: str) -> Executor:
     """Create a Globus Compute SDK client from user's access token"""
     try:
 
         # Get Globus Compute client
-        gcc = get_compute_client(user)
+        gcc = get_compute_client(user_name, user_api_key)
 
         # Create and return the executor
         return Executor(
@@ -33,16 +34,17 @@ def get_compute_executor(user: account_models.User) -> Executor:
         
     # Error if something wrong happen
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Could not create Globus Compute executor for user {user.name}: {e}")
+        raise HTTPException(status_code=500, detail=f"Could not create Globus Compute executor for user {user_name}: {e}")
 
 
 # Get Globus Compute Client
-def get_compute_client(user: account_models.User) -> Client:
+@cached(cache=TTLCache(maxsize=1024, ttl=60 * 60))
+def get_compute_client(user_name: str, user_api_key: str) -> Client:
     """Create a Globus Compute SDK client from user's access token"""
     try:
 
         # Get Globus authorizers
-        compute_auth = AccessTokenAuthorizer(user.api_key)
+        compute_auth = AccessTokenAuthorizer(user_api_key)
 
         # Create Globus login manager using tokens
         compute_login_manager = AuthorizerLoginManager(
@@ -57,7 +59,10 @@ def get_compute_client(user: account_models.User) -> Client:
     
     # Error if something wrong happen
     except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Could not create Globus Compute client for user {user.name}: {e}")
+        raise HTTPException(
+            status_code=500, 
+            detail=f"Could not create Globus Compute client for user {user_name}: {e}"
+        )
     
 
 # Get Compute endpoint ID
@@ -99,7 +104,7 @@ def submit_task_and_get_result(
     function_id = get_compute_function_id(function_name)
         
     # Get Globus Compute executor from user's token
-    gce = get_compute_executor(user)
+    gce = get_compute_executor(user.name, user.api_key)
     gce.endpoint_id = endpoint_id
 
     # Make sure the endpoint runs on the login node
@@ -153,45 +158,34 @@ async def submit_task(
     endpoint_id = get_compute_endpoint_id(resource)
     function_id = get_compute_function_id(function_name)
         
-    # Get Globus Compute executor from user's token
-    gce = get_compute_executor(user)
-    gce.endpoint_id = endpoint_id
 
-    # Make sure the endpoint runs on the login node
-    gce.user_endpoint_config = {"provider": "local"}
+    # Get Globus Compute client from user's token
+    gcc = get_compute_client(user.name, user.api_key)
+
+
 
     # Submit task to Globus Compute
     try:
-        future = gce.submit_to_registered_function(function_id, args=[input_data])
+        batch = gcc.create_batch(user_endpoint_config={"provider": "local"})
+        batch.add(function_id, [input_data])
+        batch_response = gcc.batch_run(endpoint_id, batch)
     except Exception as e:
         raise HTTPException(
             status_code=HTTP_500_INTERNAL_SERVER_ERROR,
             detail=f"Could not submit task to Globus Compute: {e}"
         )
     
-    # While the future object still does not have a task ID assigned ...
-    start = time.monotonic()
-    while future.task_id is None:
-
-        # Sleep for a little while
-        await asyncio.sleep(0.5)
-
-        # Raise an error is something went wrong
-        if future._exception:
-            raise HTTPException(
-                status_code=HTTP_500_INTERNAL_SERVER_ERROR, 
-                detail=f"Could not submit task to Globus Compute: {str(future._exception)}"
-            )
-        
-        # Raise an error if the process is taking too long
-        if time.monotonic() - start > 10:
-            raise HTTPException(
-                status_code=HTTP_408_REQUEST_TIMEOUT,
-                detail=f"Timeout while retrieving task ID from Globus Compute."
-            )
+    # Try to recover the task ID
+    try:
+        task_id = list(batch_response["tasks"].values())[0][0]
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Could not recover Globus Compute task ID: {e}"
+        )
         
     # Return the Globus Compute task ID
-    return future.task_id
+    return task_id
 
 
 # Get task status
@@ -200,7 +194,7 @@ def get_task_status(user: account_models.User, task_id: str):
     """Check the status of a task with Globus Compute and return result if completed."""
 
     # Get Globus Compute client using user's credentials
-    gcc = get_compute_client(user)
+    gcc = get_compute_client(user.name, user.api_key)
 
     # Try to get the task status from Globus
     try:
@@ -239,3 +233,61 @@ def get_task_status(user: account_models.User, task_id: str):
 
     # Return the status and result (if any)
     return status, result
+
+
+# ========= BACKUP ===========
+
+# OLD Submit task
+'''
+async def OLD_submit_task(
+        function_name: str, 
+        resource: status_models.Resource, 
+        input_data: dict, 
+        user: account_models.User
+    ) -> str:
+    """Extract endpoint and function IDs, submit task, and return task ID."""
+        
+    # Extract Globus Compute endpoint and function IDs
+    endpoint_id = get_compute_endpoint_id(resource)
+    function_id = get_compute_function_id(function_name)
+        
+    # Get Globus Compute executor from user's token
+    gce = get_compute_executor(user)
+    gce.endpoint_id = endpoint_id
+
+    # Make sure the endpoint runs on the login node
+    gce.user_endpoint_config = {"provider": "local"}
+
+    # Submit task to Globus Compute
+    try:
+        #future = gce.submit_to_registered_function(function_id, args=[input_data])
+    except Exception as e:
+        raise HTTPException(
+            status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=f"Could not submit task to Globus Compute: {e}"
+        )
+    
+    # While the future object still does not have a task ID assigned ...
+    start = time.monotonic()
+    while future.task_id is None:
+
+        # Sleep for a little while
+        await asyncio.sleep(0.5)
+
+        # Raise an error is something went wrong
+        if future._exception:
+            raise HTTPException(
+                status_code=HTTP_500_INTERNAL_SERVER_ERROR, 
+                detail=f"Could not submit task to Globus Compute: {str(future._exception)}"
+            )
+        
+        # Raise an error if the process is taking too long
+        if time.monotonic() - start > 10:
+            raise HTTPException(
+                status_code=HTTP_408_REQUEST_TIMEOUT,
+                detail=f"Timeout while retrieving task ID from Globus Compute."
+            )
+        
+    # Return the Globus Compute task ID
+    return future.task_id
+'''
