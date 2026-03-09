ec30852e2bedd95ba6506bd89217f5560894d4d0
BenoitCote <benoitcote@anlvpn039.mcs.anl.gov>
2026-03-09 15:13:27 -0700

    added indents to the json response

--- alcf/python_example_scripts/compute_get_job.py
+++ alcf/python_example_scripts/compute_get_job.py
@@ -1,3 +1,4 @@
+import json
 import os
 import requests
 from dotenv import load_dotenv
@@ -5,7 +6,7 @@ load_dotenv()
 
 # Targeted resource
 resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
-job_id = "78335"
+job_id = "79006"
 historical = True
 
 # Build headers
@@ -21,4 +22,4 @@ url = f"{os.getenv('BASE_URL')}/compute/status/{resource_id}/{job_id}?historical
 # Send request to Facility API
 response = requests.get(url, headers=headers)
 print(response.status_code)
-print(response.json())
+print(json.dumps(response.json(), indent=2))
--- alcf/python_example_scripts/compute_update_job.py
+++ alcf/python_example_scripts/compute_update_job.py
@@ -1,3 +1,4 @@
+import json
 import os
 import requests
 from dotenv import load_dotenv
@@ -5,7 +6,7 @@ load_dotenv()
 
 # Targeted resource
 resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
-job_id = "78335"
+job_id = "79006"
 
 # Build input data
 data = {
@@ -24,4 +25,4 @@ url = f"{os.getenv('BASE_URL')}/compute/job/{resource_id}/{job_id}"
 # Send request to Facility API
 response = requests.put(url, json=data, headers=headers)
 print(response.status_code)
-print(response.json())
+print(json.dumps(response.json(), indent=2))
