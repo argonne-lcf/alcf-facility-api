43404084ac6ac2161f24048b926ffa5a9bb7d8b6
BenoitCote <benoitcote@anlvpn046.mcs.anl.gov>
2026-03-10 18:03:30 -0700

    added mkdir function definition and example

---
+++ alcf/python_example_scripts/filesystem_mkdir.py
@@ -0,0 +1,19 @@
+from common_filesystem import submit
+
+# Targeted resource
+#resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
+resource_id = "9674c7e1-aecc-4dbb-bf01-c9197e027cd6" # Sophia
+
+# Build input data
+data = {
+    "path": "/home/bcote/dir_test2/hello/dir/test2",
+    "parent": True,
+}
+
+# Send request to Facility API
+submit(
+    resource_id=resource_id,
+    data=data,
+    function="mkdir",
+    method="post"
+)
