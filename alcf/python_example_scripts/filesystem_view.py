96c50b98b87fd6c7bb62121a653928fc06dd37e4
BenoitCote <benoitcote@anlvpn100.mcs.anl.gov>
2026-03-12 13:47:25 -0700

    switch filesystem resource to Eagle

--- alcf/python_example_scripts/filesystem_view.py
+++ alcf/python_example_scripts/filesystem_view.py
@@ -9,7 +9,8 @@ ACCESS_TOKEN = get_access_token()
 
 # Targeted resource
 #resource_id = "7f7d0593-162e-43b9-8476-07d7d137d6ab" # Edith
-resource_id = "9674c7e1-aecc-4dbb-bf01-c9197e027cd6" # Sophia
+#resource_id = "9674c7e1-aecc-4dbb-bf01-c9197e027cd6" # Sophia
+resource_id = "1c3ad9d4-2e91-42bc-becb-72b1fde1235c" # Eagle
 
 # Build input data
 data = {
