ba227ca318a697e64cddde44c429961953fcd536
BenoitCote <benoitcote@anlvpn099.mcs.anl.gov>
2026-03-21 13:55:19 -0700

    now using ACCESS_TOKEN from .env

--- alcf/python_example_scripts/get_task.py
+++ alcf/python_example_scripts/get_task.py
@@ -1,19 +1,15 @@
 import os
 import requests
 import json
-from globus_access_token import get_access_token
 from dotenv import load_dotenv
 load_dotenv()
 
 # Define your task ID
 task_id = "6daa409a-09a8-4b76-80ad-c9ca51e9d461"
 
-# Get Globus access token
-ACCESS_TOKEN = get_access_token()
-
 # Build headers
 headers = {
-    "Authorization": f"Bearer {ACCESS_TOKEN}",
+    "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN', None)}",
     "Content-Type": "application/json"
 }
 
