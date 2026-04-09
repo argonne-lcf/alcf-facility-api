5f753390525dad6382935eb5b90ff86c477313aa
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2026-04-09 08:31:10 -0700

    add transfer scope with data_access consent

--- alcf/python_example_scripts/simple_globus_access_token.py
+++ alcf/python_example_scripts/simple_globus_access_token.py
@@ -7,9 +7,16 @@ AUTH_CLIENT_ID = "8b84fc2d-49e9-49ea-b54d-b3a29a70cf31"
 SCOPE_CLIENT_ID = "6be511f6-a071-471f-9bc0-02a0d0836723"
 SCOPE_STRING = f"https://auth.globus.org/scopes/{SCOPE_CLIENT_ID}/filesystem"
 
+# ALCF Mapped Collection UUID
+COLLECTION_ID = "05d2c76a-e867-4f67-aa57-76edeb0beda0"
+
+# Creating our additional scope string to enable transfer operations on Mapped collection
+additional_scopes = f"urn:globus:auth:scope:transfer.api.globus.org:all[*https://auth.globus.org/scopes/{COLLECTION_ID}/data_access]"
+combined_scope=f"{SCOPE_STRING}[{additional_scopes}]"
+
 # Prepare the authentication flow with the targetted scope
 client = globus_sdk.NativeAppAuthClient(client_id=AUTH_CLIENT_ID)
-client.oauth2_start_flow(requested_scopes=[SCOPE_STRING])
+client.oauth2_start_flow(requested_scopes=[combined_scope])
 
 # Trigger the authentication flow
 auth_url = client.oauth2_get_authorize_url()
