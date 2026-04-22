c7f9298ae50ae6b877edc426af20913b7bac2fd5
BenoitCote <benoitcote@BenoitCotes-MacBook-Pro.local>
2026-04-22 09:45:34 -0700

    added example for mkdir

---
+++ alcf/python_example_scripts/filesystem_mkdir.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+
+# Targeted resource
+resource_id = "6115bd2c-957a-4543-abff-5fae52992ff2" # Home
+#resource_id = "1c3ad9d4-2e91-42bc-becb-72b1fde1235c" # Eagle
+
+# Build input data
+data = {
+    "path": "/home/bcote/iri_dir_test",
+    "parent": False
+}
+
+# Build headers
+headers = {
+    "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN', None)}",
+    "Content-Type": "application/json"
+}
+
+# Build URL
+url = f"{os.getenv('BASE_URL')}/filesystem/mkdir/{resource_id}"
+
+# Send request to Facility API
+response = requests.post(url, json=data, headers=headers)
+
+# Print response
+print(response.status_code)
+print(response.json())
