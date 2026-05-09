00a86e3509d72d775bdc3b214301fa5e96fd7de0
BenoitCote <benoitcote@anlvpn135.mcs.anl.gov>
2026-05-09 09:25:16 -0700

    added enums.py class to avoid circular imports

---
+++ alcf/enums.py
@@ -0,0 +1,12 @@
+from enum import Enum
+
+
+class APIComponent(str, Enum):
+    COMPUTE = "compute"
+    FILESYSTEM = "filesystem"
+    ACCOUNT = "account"
+
+
+class EndpointType(str, Enum):
+    PBS_GRAPHQL = "pbs_graphql"
+    GLOBUS_MULTI_USER_ENDPOINT = "globus_multi_user_endpoint"
