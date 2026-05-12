1a564bf9de7b2a4e949b4237451645495f163a52
BenoitCote <benoitcote@anlvpn177.mcs.anl.gov>
2026-05-12 08:49:20 -0700

    streamlined codes to use reusable functions

--- alcf/status/logs.py
+++ alcf/status/logs.py
@@ -1,29 +1,21 @@
 import asyncio
 import json
-import inspect
 from uuid import uuid4
 from functools import cache, wraps
-from pydantic import BaseModel, Field
-from typing import Optional, Dict, Any
-from datetime import datetime, timezone
-from starlette.status import HTTP_200_OK
-from fastapi import HTTPException
 
 from alcf.config import LOG_BASE_PATH
-from alcf.logging.async_logging import setup_structured_logger, AsyncBaseLogger
+from alcf.logging.async_logging import (
+    BaseLog,
+    AsyncBaseLogger,
+    get_input_from_func,
+    setup_structured_logger,
+    run_and_log
+)
 
 
-# Data format for the log
-class StatusLog(BaseModel):
-    id: str
-    api_route: str
-    status_code: Optional[int] = Field(default=None)
-    input: Dict[Any, Any]
-    error: Optional[str] = Field(default=None)
-    timestamp: datetime = Field(default_factory=lambda: datetime.now(timezone.utc).replace(tzinfo=None))
-    
+# Decorator to log operations
+# ===========================
 
-# Decorator to be added to every status function
 def track_status_operation(func):
     @wraps(func)
     async def wrapper(*args, **kwargs):
@@ -32,33 +24,18 @@ def track_status_operation(func):
         logger = await get_status_logger()
         
         # Gather input data
-        sig = inspect.signature(func)
-        bound_args = sig.bind(*args, **kwargs)
-        bound_args.apply_defaults()
-        input_data = dict(bound_args.arguments)
-        input_data.pop('self', None)
+        input_data = get_input_from_func(func, *args, **kwargs)
 
         # Initialize log
-        status_log = StatusLog(
+        status_log = BaseLog(
             id=str(uuid4()),
             api_route=f"status_{func.__name__}",
             input=input_data,
         )
         
         # Run operation and log after
-        try:
-            result = await func(*args, **kwargs)
-            status_log.status_code = HTTP_200_OK
-            logger.log_async(status_log)
-            return result
+        return await run_and_log(status_log, logger, func, *args, **kwargs)
         
-        # Propagate error in the log (if any)
-        except HTTPException as e:
-            status_log.status_code = e.status_code
-            status_log.error = str(e.detail)
-            logger.log_async(status_log)
-            raise
-
     return wrapper
 
 
@@ -75,12 +52,12 @@ _status_slog = setup_structured_logger(
 class AsyncStatusLogger(AsyncBaseLogger):
     """Class to write status logs to jsonl file."""
 
-    def log_async(self, status_log: StatusLog) -> None:
+    def log_async(self, status_log: BaseLog) -> None:
         task = asyncio.create_task(write_status_log(status_log))
         self._background_tasks.add(task)
         task.add_done_callback(self._on_done)
 
-async def write_status_log(status_log: StatusLog) -> None:
+async def write_status_log(status_log: BaseLog) -> None:
     _status_slog.info(json.dumps(status_log.model_dump(mode="json")))
 
 @cache
