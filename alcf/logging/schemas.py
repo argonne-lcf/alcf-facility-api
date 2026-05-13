dd7b3faef9265a2e19a7a57ba3f47ae8c8db9694
BenoitCote <benoitcote@anlvpn180.mcs.anl.gov>
2026-05-13 09:01:27 -0700

    reorganized pydantic models

--- alcf/logging/schemas.py
+++ alcf/logging/schemas.py
@@ -1,12 +1,15 @@
 from pydantic import BaseModel, Field
 from datetime import datetime, timezone
 from typing import Dict, Any, Optional
+from uuid import uuid4
 
+
+# Non-authenticated base models
+# =============================
 
 class BaseLog(BaseModel):
-    id: str
-    stream: str
-    api_route: str
+    id: Optional[str] = Field(default=str(uuid4()))
+    api_function: str
     status_code: Optional[int] = Field(default=None)
     input: Dict[Any, Any]
     error: Optional[str] = Field(default=None)
@@ -15,23 +18,51 @@ class BaseLog(BaseModel):
     @classmethod
     def has_field(cls, field_name: str) -> bool:
         return field_name in cls.model_fields
-    
 
-class AuthenticatedBaseLog(BaseLog):
+
+class BaseLogWithResponse(BaseLog):
+    response: Optional[Dict[Any, Any]] = Field(default=None)
+
+
+# Authenticated base models
+# =========================
+
+class AutheBaseLog(BaseLog):
     user_id: str
     user_name: str
     ip: str
 
 
-class AuthenticateComputeLog(AuthenticatedBaseLog):
+class AuthBaseLogWithResponse(AutheBaseLog):
+    response: Optional[Dict[Any, Any]] = Field(default=None)
+
+
+# API component log models
+# ========================
+
+class StatusLog(BaseLog):
+    stream: Optional[str] = Field(default="status")
+
+
+class FacilityLog(BaseLog):
+    stream: Optional[str] = Field(default="facility")
+
+
+class AuthComputeLog(AuthBaseLogWithResponse):
     resource_id: str
     alcf_username: str
-    response: Optional[Dict[Any, Any]] = Field(default=None)
+    stream: Optional[str] = Field(default="compute")
 
 
-class AccountLog(BaseLog):
-    response: Optional[Dict[Any, Any]] = Field(default=None)
+class AuthFilesystemLog(AuthBaseLogWithResponse):
+    resource_id: str
+    stream: Optional[str] = Field(default="filesystem")
 
 
-class AuthenticatedAccountLog(AuthenticatedBaseLog):
-    response: Optional[Dict[Any, Any]] = Field(default=None)
+class AccountLog(BaseLogWithResponse):
+    stream: Optional[str] = Field(default="account")
+
+
+class AuthAccountLog(AuthBaseLogWithResponse):
+    stream: Optional[str] = Field(default="account")
+
