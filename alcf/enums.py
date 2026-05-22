ca594df30b448bf2ab17f8d5b2e61b9e0548ae8b
BenoitCote <benoitcote@anlvpn058.mcs.anl.gov>
2026-05-22 08:20:26 -0700

    added ni_rest_api as an endpoint option

--- alcf/enums.py
+++ alcf/enums.py
@@ -10,3 +10,8 @@ class APIComponent(str, Enum):
 class EndpointType(str, Enum):
     PBS_GRAPHQL = "pbs_graphql"
     GLOBUS_MULTI_USER_ENDPOINT = "globus_multi_user_endpoint"
+    NI_REST_API = "ni_rest_api"
+
+
+class AllType(str, Enum):
+    ALL = "all"
