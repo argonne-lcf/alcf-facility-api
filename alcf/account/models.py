31e53324ef97884dd63905ef9f4e879b23e39e8b
BenoitCote <benoitcote@anlvpn058.mcs.anl.gov>
2026-05-22 08:22:46 -0700

    added pydantic classes for the Ni REST API responses

---
+++ alcf/account/models.py
@@ -0,0 +1,71 @@
+from datetime import datetime, timezone
+from pydantic import ValidationError, BaseModel, field_validator, Field
+from typing import List, Optional
+
+from app.routers.account.models import AllocationUnit
+
+
+class NiProject(BaseModel):
+    id: str
+    name: str
+    users: List[str]
+    resources: List[str]
+    description: Optional[str] = Field(default="N/A")
+    last_updated: Optional[datetime] = Field(default=datetime(2000, 1, 1, 0, 0, 0, tzinfo=timezone.utc))
+
+
+class NiAllocation(BaseModel):
+    allocation_id: str
+    resource: str
+    project: str
+    balance: int
+    deposits: int
+
+    @property
+    def used(self) -> int:
+        return self.deposits - self.balance
+    
+    @field_validator("allocation_id", mode="before")
+    @classmethod
+    def format_allocation_id(cls, value):
+        return str(value)
+    
+
+class NiCapability(BaseModel):
+    name: str
+    units: List[str]
+    description: Optional[str] = Field(default="N/A")
+    last_updated: Optional[datetime] = Field(default=datetime(2000, 1, 1, 0, 0, 0, tzinfo=timezone.utc))
+
+    @field_validator("last_updated", mode="before")
+    @classmethod
+    def format_last_updated(cls, value):
+        if isinstance(value, str) and value.startswith("datetime::"):
+            value = value.removeprefix("datetime::")
+        dt = datetime.fromisoformat(value)
+        return dt.replace(tzinfo=timezone.utc)
+
+    @field_validator("units", mode="after")
+    @classmethod
+    def format_units(cls, value):
+        formatted_units = []
+        for ni_unit in value:
+            if ni_unit == "Node Hours":
+                formatted_units.append(AllocationUnit.node_hours.value)
+            elif ni_unit == "TB": # TODO: need to handle the conversion, add TB in IRI, or receive bytes
+                formatted_units.append(AllocationUnit.bytes.value)
+            else:
+                raise ValidationError(f"Cannot format allocation unit. {ni_unit} not supported.")
+        return formatted_units
+    
+
+class NiProjects(BaseModel):
+    projects: List[NiProject]
+
+
+class NiAllocations(BaseModel):
+    allocations: List[NiAllocation]
+
+
+class NiCapabilities(BaseModel):
+    capabilities: List[NiCapability]
---
+++ alcf/httpx_client.py
@@ -0,0 +1,52 @@
+from typing import Any
+import httpx
+from fastapi import HTTPException
+
+class AsyncHttpClient:
+    
+    def __init__(
+        self,
+        timeout: float = 30.0,
+        headers: dict[str, str] | None = None,
+    ):
+        if headers is None:
+            headers = {"Content-Type": "application/json"}
+        self.headers = headers
+        self._client = httpx.AsyncClient(timeout=timeout, headers=self.headers)
+
+    def _handle_error(self, url: str, e: Exception) -> None:
+        if isinstance(e, httpx.HTTPStatusError):
+            raise HTTPException(
+                detail=f"Upstream endpoint returned {e.response.status_code}: {e.response.content[:256]!r}.",
+                status_code=e.response.status_code,
+            )
+        elif isinstance(e, httpx.TimeoutException):
+            raise HTTPException(
+                details=f"Request timeout.",
+                status_code=504,
+            )
+        elif isinstance(e, httpx.HTTPError):
+            raise HTTPException(
+                detail=f"HTTP error calling API at {url}: {e}",
+                status_code=500
+            )
+        raise e
+
+    async def post(self, url: str, data: Any = None) -> Any:
+        try:
+            response = await self._client.post(url, json=data)
+            response.raise_for_status()
+            return response.json()
+        except (httpx.HTTPStatusError, httpx.TimeoutException, httpx.HTTPError) as e:
+            self._handle_error(url, e)
+
+    async def get(self, url: str) -> Any:
+        try:
+            response = await self._client.get(url)
+            response.raise_for_status()
+            return response.json()
+        except (httpx.HTTPStatusError, httpx.TimeoutException, httpx.HTTPError) as e:
+            self._handle_error(url, e)
+
+    async def close(self) -> None:
+        await self._client.aclose()
