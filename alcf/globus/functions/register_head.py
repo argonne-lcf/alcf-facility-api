f5bce8e70dd5a95f57d943f95411c0a5d985ecc0
BenoitCote <benoitcote@anlvpn058.mcs.anl.gov>
2026-05-22 09:01:11 -0700

    light edit to error message.

--- alcf/globus/functions/register_head.py
+++ alcf/globus/functions/register_head.py
@@ -147,7 +147,7 @@ def head(params):
 
     # Check if path is a hidden file
     if input_data.path.name.startswith("."):
-        return Response(error=f"Cannot view hidden file {input_data.path}.").model_dump()
+        return Response(error=f"Accessing content of hidden files is forbidden. {input_data.path}.").model_dump()
 
     # Check if O_NOFOLLOW is supported (TOCTOU protection)
     o_nofollow = getattr(os, "O_NOFOLLOW", None)
