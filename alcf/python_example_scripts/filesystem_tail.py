14872d6050f10ca3bcbf07219a54142ec191fe95
BenoitCote <benoitcote@anlvpn083.mcs.anl.gov>
2026-08-25 11:28:02 -0700

    added python examples for checksum, file, tail

---
+++ alcf/python_example_scripts/filesystem_tail.py
@@ -0,0 +1,30 @@
+import os
+import requests
+from dotenv import load_dotenv
+load_dotenv(override=True)
+
+# Targeted resource
+resource_id = "6115bd2c-957a-4543-abff-5fae52992ff2" # Home
+#resource_id = "1c3ad9d4-2e91-42bc-becb-72b1fde1235c" # Eagle
+
+# Build input data
+data = {
+    "path": "/home/bcote/test.txt",
+    "lines": 33
+}
+
+# Build headers
+headers = {
+    "Authorization": f"Bearer {os.getenv('ACCESS_TOKEN', None)}",
+    "Content-Type": "application/json"
+}
+
+# Build URL
+url = f"{os.getenv('BASE_URL')}/filesystem/tail/{resource_id}"
+
+# Send request to Facility API
+response = requests.get(url, params=data, headers=headers)
+
+# Print response
+print(response.status_code)
+print(response.json())
